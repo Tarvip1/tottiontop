ae641f47c00e946e160c6fdcbad6756b78f916a6
Tarvip1 <saruregency@gmail.com>
2025-09-06 12:53:18 +0700

    Create proxy.js

---
+++ api/proxy.js
@@ -0,0 +1,9 @@
+import fetch from "node-fetch";
+
+export default async function handler(req, res) {
+  const url = "https://totti911-fastx.store" + req.url.replace("/api/proxy", "");
+  const response = await fetch(url);
+  const data = await response.text();
+  res.setHeader("Content-Type", "text/html");
+  res.status(200).send(data);
+}
