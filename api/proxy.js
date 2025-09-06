67534041f9d927b33c737d2c29154dc97d99ca10
Tarvip1 <saruregency@gmail.com>
2025-09-06 12:45:22 +0700

    Delete api directory

--- api/proxy.js
+++
@@ -1,9 +0,0 @@
-import fetch from "node-fetch";
-
-export default async function handler(req, res) {
-  const url = "https://totti911-sun.online" + req.url.replace("/api/proxy", "");
-  const response = await fetch(url);
-  const data = await response.text();
-  res.setHeader("Content-Type", "text/html");
-  res.status(200).send(data);
-}
